3db39b2b224844d64f9e8d5bbbe8f919851f2621
sosthene14 <sosthenemounsambote14@gmail.com>
2024-06-02 00:25:06 +0000

    :feat: improved seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,6 @@ export default defineConfig({
     skipInline: false,
     drafts: true
   },
-  site: 'https://lexingtonthemes.com',
+  site: 'https://zen-vie-blog.vercel.app',
   integrations: [tailwind(), sitemap(), mdx()]
 });
